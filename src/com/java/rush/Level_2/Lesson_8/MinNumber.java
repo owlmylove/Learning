83a87e7626e1b1d99c111253d206d82c12df02cd
Lilit Gevorgyan <lilitturbizinfo@gmail.com>
2018-05-25 00:26:39 +0300

    min numbers files changed

--- src/com/java/rush/Level_2/Lesson_8/MinNumber.java
+++ src/com/java/rush/Level_2/Lesson_8/MinNumber.java
@@ -2,25 +2,22 @@ package com.java.rush.Level_2.Lesson_8;
 
 public class MinNumber {
     public static int min(int a, int b, int c) {
-
-        if (a < b && b < c) {
+        if (a < b && a <= c){
             return a;
-        } else if (c < b && c < a) {
+        }
+        else if(c < a && c < b){
             return c;
-        } else if (a < b && c < b) {
-            return a;
-        } else if (b < a && b < c) {
+        }
+        else{
             return b;
-        } else {
-            return 0;
         }
 
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(min(1, 2, 3));
-        System.out.println(min(-1, -2, -3));
-        System.out.println(min(3, 5, 3));
+        System.out.println(min(1, 2, 3));//
+        System.out.println(min(-1, -2, -3));//
+        System.out.println(min(3, 5, 3));//
         System.out.println(min(10, 5, 10));
     }
 
