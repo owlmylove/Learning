9e0e0466d5bb3e0130509a5da9438c7f63949295
Lilit Gevorgyan <lilitturbizinfo@gmail.com>
2018-05-26 01:07:23 +0300

    added divide class and Level_3

---
+++ src/com/java/rush/Level_3/Lesson_1/Divide.java
@@ -0,0 +1,16 @@
+package com.java.rush.Level_3.Lesson_1;
+
+public class Divide {
+        public static void main(String[] args) {
+            div(6, 3);
+            div(10, 6);
+            div(2, 4);
+        }
+
+        public static void div(int a, int b) {
+            int c;
+            c = (a / b);
+            System.out.println(c);
+        }
+    }
+
